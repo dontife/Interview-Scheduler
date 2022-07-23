0c8dbd9a98b80dd25a46945f54b27048bf66ea55
Boluwatife Ade ogunlola <dontife@ymail.com>
2022-07-23 04:16:32 -0400

    create DayListItem component and implement the different states using storybook

--- stories/index.js
+++ stories/index.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import DayListItem from "../src/components/DayListItem"
 
 import "index.scss";
 
@@ -22,3 +23,18 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
+
+  // Initiates storybook and register our DayList component
+storiesOf("DayListItem", module)
+  // provides the default background color for our component
+  .addParameters({
+    backgrounds: [{name: "dark", value:"#222f3e", default:true }]
+  })
+  // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Unselected", () => <DayListItem name ="Monday" spots={5} />)
+  .add("Selected", () => <DayListItem name ="Monday" spots={5} selected />)
+  .add("Full", () => <DayListItem name ="Monday" spots={0} />)
+  .add("Clickable", () => <DayListItem name ="Tuesday" setDay={action("setDay")} spots={5} />)
+
+
+  
