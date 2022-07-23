910de397362080482dcf0cfd5925871bcba1ba4f
Boluwatife Ade ogunlola <dontife@ymail.com>
2022-07-23 05:23:34 -0400

    style the dailyListItem components dependning on the class of the component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import classNames from 'classnames'
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+    const listClass = classNames ('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': props.spots === 0 && true});
     return (
-        <li onClick={() => props.setDay(props.name)}>
-          <h2 className="text--regular"> {props.name}</h2>
+        <li className={listClass} onClick={() => props.setDay(props.name)}>
+          <h2 className="text--regular">{props.name}</h2>
           <h3 className="text--light">{props.spots} spots remaining</h3>
-            Set Day
          
         </li>
     );
--- stories/index.js
+++ stories/index.js
@@ -31,7 +31,7 @@ storiesOf("DayListItem", module)
     backgrounds: [{name: "dark", value:"#222f3e", default:true }]
   })
   // To define our stories, we call add() once for each of our test states to generate a story
-  .add("Unselected", () => <DayListItem name ="Monday" spots={5} />)
+  .add("Unselected", () => <DayListItem  name ="Monday" spots={5} />)
   .add("Selected", () => <DayListItem name ="Monday" spots={5} selected />)
   .add("Full", () => <DayListItem name ="Monday" spots={0} />)
   .add("Clickable", () => <DayListItem name ="Tuesday" setDay={action("setDay")} spots={5} />)
