284e341454e8a90a25af92d6d65b48e223bf7691
Boluwatife Ade ogunlola <dontife@ymail.com>
2022-07-24 14:12:26 -0400

    refactor InterviewList and InterviewListItem component by reducing the number of props passed

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -7,7 +7,7 @@ import "components/InterviewerListItem.scss"
 export default function InterviewerListItem (props) {
     const interviewerClass = classNames("interviewers__item", {"interviewers__item--selected": props.selected})
     return (
-      <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
+      <li className={interviewerClass} onClick={props.setInterviewer}>
         <img
          className="interviewers__item-image"
          src={props.avatar}
